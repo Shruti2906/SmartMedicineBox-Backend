bed99578e807e376bd2139da352546c1d89dae70
Shruti Patil <65824891+Shruti2906@users.noreply.github.com>
2023-12-15 23:26:06 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,13 @@
+1. Create a `.env` file with the following options
+```
+APP_PORT=3000
+DB_USER=<DB_USER>
+DB_PASS=<DB_PASS>
+DB_HOST=<DB_HOST>
+DB_PORT=<DB_PORT>
+JWT_SECRET=<JWT_SECRET>
+```
+
+2. Run `npm i`
+
+3. Run `node server.js`
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const { APP_PORT } = require('./api/utils/constants')
+const userRoutes = require('./api/routes/user.route')
+const app = express()
+
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if (req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+});
+
+app.get('/', (req, res) => {
+    res.send(new Date());
+})
+
+app.use('/user', userRoutes)
+
+
+app.use((req, res, next) => {
+    const error = new Error("Not found");
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    });
+});
+
+app.listen(APP_PORT, () => {
+    console.log(`Example app listening on port ${APP_PORT}`)
+})
